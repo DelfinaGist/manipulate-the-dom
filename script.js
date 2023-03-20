db8a4ee23e9787c23027bdc996d652b5d3d9ac54
DelfinaGist <fullstackcodez@gmail.com>
2023-03-19 21:56:06 -0500

    message

--- script.js
+++ script.js
@@ -17,66 +17,68 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-const myTitle = document.querySelector('#main-title');
-myTitle.innerHTML = '<h1>"The DOM"</h1>'
-// console.log(newTitle);
+  const myTitle = document.querySelector('#main-title');
+  myTitle.innerHTML = '<h1>"The DOM"</h1>'
+  // console.log(newTitle);
 
   // Part 2
-const bodyEl = document.querySelector('body')
-bodyEl.style.backgroundColor = 'orange';
-//change the background-color
+  const bodyEl = document.querySelector('body')
+  bodyEl.style.backgroundColor = 'orange';
+  //change the background-color
 
   // Part 3
-const favoriteThings = document.querySelector('#favorite-things');
-favoriteThings.removeChild(favoriteThings.lastElementChild);
-//removed last item on DOM's favorite things list 
+  const favoriteThings = document.querySelector('#favorite-things')
+  favoriteThings.removeChild(favoriteThings.lastElementChild);
+  //removed last item on DOM's favorite things list 
 
   // Part 4
-const specTitles = document.querySelectorAll(".special-title")
-for (const titles of specTitles) {
+  const specTitles = document.querySelectorAll(".special-title")
+  for (const titles of specTitles) {
   titles.style.fontSize = '2rem';
-}
+  }
 
   // Part 5
-const pastRaces = document.querySelector("#past-races");
-pastRaces.removeChild(pastRaces.children[3]);
-//remove Chicago from past races list
+  const pastRaces = document.querySelector("#past-races");
+  pastRaces.removeChild(pastRaces.children[3]);
+  //remove Chicago from past races list
 
   // Part 6
-const newLi = doecument.createElement('li');
-newLi.textContent = "Monte-Carlo"
-pastRaces.appendChild(newLi);
-//add new city to past races list
+  const newLi = document.createElement('li');
+  newLi.textContent = 'Monte-Carlo'
+  pastRaces.appendChild(newLi);
+  //add new city to past races list
 
   // Part 7
-const newBlogPost = document.createElement('div');
-newBlogPost.classList.add('blog-post')
+  const newBlogPost = document.createElement('div');
+  newBlogPost.classList.add('blog-post')
 
-const newHeading = document.createElement('h1')
-newHeading.Content = "Monte-Carlo"
+  const newHeading = document.createElement('h1')
+  newHeading.Content = "Monte-Carlo"
 
-const NewP = document.createElement('p');
-NewP.textContent = "Faster, faster, faster until the thrill of speed overcomes the fear of death!"
+  const newP = document.createElement('p');
+  newP.textContent = "Faster, faster, faster until the thrill of speed overcomes the fear of death!"
 
-newBlogPost.appendChild(newHeading);
-newBlogPost.appendChild(NewP);
+  newBlogPost.appendChild(newHeading);
+  newBlogPost.appendChild(newP);
 
-var blogPostsSection = document.querySelector(".main");
-blogPostsSection.appendChild(newBlogPost);
+  var blogPostsSection = document.querySelector(".main");
+  blogPostsSection.appendChild(newBlogPost);
 
   // Part 8
-const quote = document.querySelector('#quote-title')
-quote.addEventListener('click', randomQuote)
+  const quote = document.querySelector('#quote-title')
+  quote.addEventListener('click', randomQuote)
 
   // Part 9
-const blogPosts = doecument.querySelectorAll('.blog-post');
+  const blogPosts = document.querySelectorAll('.blog-post');
 
-blogPosts.forEach(post => {
-  post.addEventListener('mouseout', () => {
-    post.classList.toggle('purple');
+  blogPosts.forEach(post => {
+    post.addEventListener('mouseout', () => {
+      post.classList.toggle('purple');
+    });
+  
+    post.addEventListener('mouseenter', () => {
+     post.classList.toggle('red');
+    });
   });
-  post.addEventListener('mouseenter', () => {
-    post.classList.toggle('red');
-  });
-});
+
 });
