e022ebbe02e29050aab1ed0aa450a063b42283fb
DelfinaGist <fullstackcodez@gmail.com>
2023-03-19 20:27:58 -0500

    message

--- script.js
+++ script.js
@@ -24,28 +24,59 @@ myTitle.innerHTML = '<h1>"The DOM"</h1>'
   // Part 2
 const bodyEl = document.querySelector('body')
 bodyEl.style.backgroundColor = 'orange';
+//change the background-color
 
   // Part 3
-
+const favoriteThings = document.querySelector('#favorite-things');
+favoriteThings.removeChild(favoriteThings.lastElementChild);
+//removed last item on DOM's favorite things list 
 
   // Part 4
-
+const specTitles = document.querySelectorAll(".special-title")
+for (const titles of specTitles) {
+  titles.style.fontSize = '2rem';
+}
 
   // Part 5
-
+const pastRaces = document.querySelector("#past-races");
+pastRaces.removeChild(pastRaces.children[3]);
+//remove Chicago from past races list
 
   // Part 6
-
+const newLi = doecument.createElement('li');
+newLi.textContent = "Monte-Carlo"
+pastRaces.appendChild(newLi);
+//add new city to past races list
 
   // Part 7
+const newBlogPost = document.createElement('div');
+newBlogPost.classList.add('blog-post')
 
+const newHeading = document.createElement('h1')
+newHeading.Content = "Monte-Carlo"
+
+const NewP = document.createElement('p');
+NewP.textContent = "Faster, faster, faster until the thrill of speed overcomes the fear of death!"
+
+newBlogPost.appendChild(newHeading);
+newBlogPost.appendChild(NewP);
+
+var blogPostsSection = document.querySelector(".main");
+blogPostsSection.appendChild(newBlogPost);
 
   // Part 8
-
+const quote = document.querySelector('#quote-title')
+quote.addEventListener('click', randomQuote)
 
   // Part 9
+const blogPosts = doecument.querySelectorAll('.blog-post');
 
-
-
-
+blogPosts.forEach(post => {
+  post.addEventListener('mouseout', () => {
+    post.classList.toggle('purple');
+  });
+  post.addEventListener('mouseenter', () => {
+    post.classList.toggle('red');
+  });
+});
 });
